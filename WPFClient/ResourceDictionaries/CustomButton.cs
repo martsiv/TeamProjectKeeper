c3714fdbd18ce79b0fb5c019d6963df7a50bfb85
Eg0rik <egoruchstroganov07@gmail.com>
2023-10-25 17:50:31 +0300

    Add button style, add icon for button, update view

---
+++ WPFClient/ResourceDictionaries/CustomButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Ext
+{
+    public class CustomButton
+    {
+      
+        public static readonly DependencyProperty ImageProperty;
+        public static ImageSource GetImage(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(ImageProperty);
+        }
+        public static void SetImage(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(ImageProperty, value);
+        }
+        static CustomButton()
+        {
+            var metadata = new FrameworkPropertyMetadata((ImageSource)null);
+            ImageProperty = DependencyProperty.RegisterAttached("Image",
+                                                                typeof(ImageSource),
+                                                                typeof(CustomButton), metadata);
+        }
+    }
+}
